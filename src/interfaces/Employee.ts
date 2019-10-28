a104b5f14ad0b01b49846e14185cdc1ff7146590
JP Barbosa <jp@jp7.com.br>
2019-10-28 19:02:07 -0300

    Add interface to NewEmployee (without id)

--- src/interfaces/Employee.ts
+++ src/interfaces/Employee.ts
@@ -1,6 +1,9 @@
-export interface Employee {
-  id: number;
+export interface NewEmployee {
   name: string;
   salary: number;
   admission: Date;
+}
+
+export interface Employee extends NewEmployee {
+  id: number;
 }
